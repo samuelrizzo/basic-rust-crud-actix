07d11bc26749bcfe678ea90ce7c62422023a98da
samuelrizzo <samuelrizzo5@gmail.com>
2024-05-18 01:15:26 -0300

    feat: IP e PORT no env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{ get, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
+use serde_json::Number;
 
 mod databases {
     pub mod postgres_connection;
@@ -11,11 +12,20 @@ async fn index() -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let _pool = databases::postgres_connection::start_connection().await;
-    HttpServer::new(||{
+    let ip: String = std::env::var("IP").unwrap_or("127.0.0.1".to_string());
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .expect("Failed to parse port");
+    HttpServer::new(|| {
         App::new()
-        .service(index)
-    }).bind(("127.0.0.1", 8080))?.run().await
+            .service(index)
+    })
+    .bind((ip, port))?
+    .run()
+    .await
 }
+
